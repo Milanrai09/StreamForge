c9fcebac5592d47dca8e3e750b8487dae20d0840
Milanrai09 <111414440+Milanrai09@users.noreply.github.com>
2025-11-29 05:12:46 +0530

    add a console log in the testing api endpoint

--- app/api/testing/route.js
+++ app/api/testing/route.js
@@ -1,10 +1,10 @@
 // app/api/test/route.js
 
 export async function GET(request) {
+    console.log('helo form testing')
     return Response.json({
       message: "Test endpoint working!",
       success: true,
       timestamp: Date.now(),
     });
   }
-  
