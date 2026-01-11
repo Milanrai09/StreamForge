d7b16f7896c65d0ce610b5ca438e2e0ff1806efc
Milan Rai <111414440+Milanrai09@users.noreply.github.com>
2026-01-11 19:53:47 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       hello world 
       <UploadVideo/>
-      <WatchVideoPage/>
+
     </div>
   );
 }
